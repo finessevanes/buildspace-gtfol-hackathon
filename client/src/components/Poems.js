7a5442ef070f4011e05c3bfefcf704df7ae8729c
Vanessa <vanessa.mercado24@gmail.com>
2022-05-30 02:02:52 -0700

    wip

--- client/src/components/Poems.js
+++ client/src/components/Poems.js
@@ -9,8 +9,8 @@ const Poems = ({ allPosts, handleDownVote, handleUpVote }) => {
             <div key={index} className={stickyNote}>
               Message: {post.message}
               <p>{post.voteCount}</p>
-              <button className='cursor-pointer' value={index} onClick={handleUpVote}>🔥</button>
-              <button className='cursor-pointer' value={index} onClick={handleDownVote}>💩</button>
+              <button className='cursor-pointer text-6xl' value={index} onClick={handleUpVote}>🔥</button>
+              <button className='cursor-pointer text-6xl' value={index} onClick={handleDownVote}>💩</button>
             </div>
           )
         })}
