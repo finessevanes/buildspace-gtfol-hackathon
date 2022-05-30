21a5e7a55c85e00931921c9dd050b799beb71fea
Vanessa <vanessa.mercado24@gmail.com>
2022-05-29 17:37:20 -0700

    moved styles into own page

---
+++ client/src/App.styles.js
@@ -0,0 +1,39 @@
+export const container = `
+flex
+w-screen
+h-screen
+object-center
+bg-synthwave
+bg-cover
+bg-center
+flex-col 
+items-center
+`
+export const stickyNote = `
+text-center
+h-40
+w-44
+bg-rose-400
+p-7
+rounded-md
+shadow-xl
+mr-4
+mb-4
+`
+export const stickynoteContainer = `
+md:grid
+md:grid-cols-5
+md:justify-center
+overflow-auto
+`
+
+export const buttonStyle = `
+bg-yellowbutton
+hover:bg-yellow-100
+text-buttontext
+font-bold
+py-2
+px-4
+rounded-full
+mb-4
+mt-4`
