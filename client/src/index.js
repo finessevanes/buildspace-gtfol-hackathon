861b0e7e536fa8ec8bafffbd9af18d9d359e3303
Vanessa <vanessa.mercado24@gmail.com>
2022-05-31 16:30:41 -0700

    added routing

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const desiredChainId = ChainId.Polygon;
 
 ReactDOM.render(
   <ThirdwebProvider desiredChainId={desiredChainId}>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
     </ThirdwebProvider>
   , document.getElementById('root'));
